dab1fa086359e26523ea5b97bb325a03986a3ed7
americanboy1422 <jphreaner142@gmail.com>
2023-01-09 17:07:12 -0800

    few things

--- test.rb
+++ test.rb
@@ -1,18 +1,24 @@
 
-def age_check(birth)
-    curr_time = Time.new
-    age_comp = curr_time.year - 18
-    date = birth.split('-')
-    if date[0].to_i < age_comp 
-        return true
-    elsif date[0].to_i == age_comp
-        if curr_time.month > date[1].to_i
-            return true
-        elsif curr_time.month == date[1].to_i && curr_time.day <= date[2].to_i
-            return true
-        end
-    else
-        return false
+def time_sence(seconds)
+    seconds.to_f
+    check = 1.0.next_float
+    if seconds.to_f/60 < check
+        return seconds.to_i.to_s + "s"
+    elsif seconds.to_f/(60*60) < check
+        return (seconds/60.to_i).to_s + 'm'
+    elsif seconds.to_f/(60*60*24) < check
+        puts('triped h')
+        return (seconds/(60*60).to_i).to_s + 'h'
+        
+    elsif (seconds.to_f/(60*60*24*7)) < check
+        puts('triped d')
+        return (seconds/(60*60*24).to_i).to_s + 'd'
+    else 
+        return (seconds/(60*60*24*7).to_i).to_s + 'w'
     end
 end
 
+num=89367
+puts(time_sence(num))
+puts((num.to_f/(60*60*24*7)))
+puts(1.0.next_float)
